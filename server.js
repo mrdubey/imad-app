a9d99c398a6eaa785b5afafaadf94476dec407ab
mrdubey <35167336+mrdubey@users.noreply.github.com>
2018-03-13 18:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req,res){
     //articleName=article-one
     //artcilesName=req.params.articles[articleName]
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
