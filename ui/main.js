193afddf69ca9c2712c975e9769920e2b8d4aba3
mrdubey <35167336+mrdubey@users.noreply.github.com>
2018-03-13 19:19:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,6 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new change";
+//change in image
+var i=document.getElementById('madi');
+i.onclick=function
