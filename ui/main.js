dad04236e40428a678e4b9c8beef3dc465d3fb4c
mrdubey <35167336+mrdubey@users.noreply.github.com>
2018-03-13 21:02:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,14 @@ var element=document.getElementById('main-text');
 element.innerHTML="new change";
 //change in image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function()
 {
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveLeft,100);
+
 };
