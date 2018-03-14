1050c4e06390f640997b83481ec423895777698d
mrdubey <35167336+mrdubey@users.noreply.github.com>
2018-03-14 12:57:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var names=[];
 app.get('/submit-name/:name',function(req,res){
     //get the current name form the request
     var name=req.params.name;//TODO
