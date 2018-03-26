f9f308be822bff7b2c08b88041eea4136cd3a23b
mrdubey <35167336+mrdubey@users.noreply.github.com>
2018-03-26 19:39:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool
 
+var config={
+    user:"sndpdby28",
+    database:"sndpdby28",
+    host:"db.hasura-app.io",
+    port:"5432",
+    password : process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -41,6 +49,21 @@ var articles =
 </p>`
     }
 };
+var pool=new Pool(config);
+app.get('/test-db',function(req,res)
+{
+    pool.query("SELECT * FROM test",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    })
+    
+});
+
 function createTemplate(data)
 {
     var title=data.title;
